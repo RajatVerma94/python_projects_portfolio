28a2fa4e3f26ea2d6eb8549c419acb937d0f4624
RajatVerma94 <rajat.work.verma@gmail.com>
2026-04-23 23:38:52 +0530

    Finished 1st project - Protagonist Name Generator

---
+++ protagonist_name_generator/task.py
@@ -0,0 +1,4 @@
+print("Welcome to the Protagonist Name Generator!")
+first_name = input("Enter the name of your first pet: ")
+last_name = input("Enter the last name of your favourite athlete: ")
+print("The name of the protagonist of your story is: " + first_name + " " + last_name)
